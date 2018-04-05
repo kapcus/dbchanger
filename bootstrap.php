21067605fa00c696959bc9f62b6d09394db1f4b9
Jan Kapciar <jan.kapciar@dhl.com>
2018-04-06 00:55:23 +0200

    config.local.neon check version 0.4

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,17 @@ require __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Console\Application;
 
 $loader = new Nette\DI\ContainerLoader(__DIR__ . '/temp', IS_DEBUG_MODE);
+if (!is_file(__DIR__ . '/config.local.neon')) {
+	echo 'Move dbchanger/misc/config.local.neon.example into dbchanger/config.local.neon first.';
+	exit(1);
+}
 $class = $loader->load(function($compiler) {
 	$compiler->loadConfig(__DIR__ . '/config/config.neon');
 	$compiler->loadConfig(__DIR__ . '/config.local.neon');
 });
 $container = new $class;
 
-$application = new Application('DbChanger', '0.3.0');
+$application = new Application('DbChanger', '0.4.0');
 $application->add($container->getByType(Kapcus\DbChanger\Command\CheckCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\MarkCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\InitCommand::class));
