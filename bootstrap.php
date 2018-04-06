5b832085107f107d4dcfce4d7e1e1aeb6efffeca
Jan Kapciar <jan.kapciar@dhl.com>
2018-04-06 02:06:30 +0200

    version change

--- bootstrap.php
+++ bootstrap.php
@@ -23,7 +23,7 @@ $class = $loader->load('', function($compiler) {
 });
 $container = new $class;
 
-$application = new Application('DbChanger', '0.4.2');
+$application = new Application('DbChanger', '0.4.3');
 $application->add($container->getByType(Kapcus\DbChanger\Command\CheckCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\MarkCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\InitCommand::class));
