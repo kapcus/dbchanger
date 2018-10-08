d1eaa77222bb361db4cbf19a732d926b8d29060e
Jan Kapciar <jan.kapciar@dhl.com>
2018-10-08 18:39:43 +0200

    (TAG 0.5.6)

--- bootstrap.php
+++ bootstrap.php
@@ -9,12 +9,12 @@ if (!is_file(__DIR__ . '/config.local.neon')) {
 	echo 'Move dbchanger/misc/config.local.neon.example into dbchanger/config.local.neon first.';
 	exit(1);
 }
-/* NETTE DI >= 2.4
-$class = $loader->load(function($compiler) {
+// NETTE DI >= 2.4
+/*$class = $loader->load(function($compiler) {
 	$compiler->loadConfig(__DIR__ . '/config/config.neon');
 	$compiler->loadConfig(__DIR__ . '/config.local.neon');
-});
-*/
+});*/
+
 
 // NETTE DI == 2.3
 $class = $loader->load('', function($compiler) {
@@ -23,7 +23,7 @@ $class = $loader->load('', function($compiler) {
 });
 $container = new $class;
 
-$application = new Application('DbChanger', '0.5.5');
+$application = new Application('DbChanger', '0.5.6');
 $application->add($container->getByType(Kapcus\DbChanger\Command\CheckCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\MarkCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\InitCommand::class));
