9f4a187c982a31337cc2b951a9dcd8f1d66cb875
Jan Kapciar <jan.kapciar@dhl.com>
2018-10-30 16:11:09 +0100

    (TAG 0.5.9)

--- bootstrap.php
+++ bootstrap.php
@@ -23,7 +23,7 @@ $class = $loader->load('', function($compiler) {
 });
 $container = new $class;
 
-$application = new Application('DbChanger', '0.5.8');
+$application = new Application('DbChanger', '0.5.9');
 $application->add($container->getByType(Kapcus\DbChanger\Command\CheckCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\MarkCommand::class));
 $application->add($container->getByType(Kapcus\DbChanger\Command\InitCommand::class));
